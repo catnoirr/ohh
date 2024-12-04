fdb3e0eeada019ac85b82e8cab1bd11a779e1730
offxsagr <hakies.06@gmail.com>
2024-12-04 13:38:13 +0530

    fixed

--- app/(auth)/home/components/JoinWhatsapp.jsx
+++ app/(auth)/home/components/JoinWhatsapp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoLogoWhatsapp } from 'react-icons/io';
 
 const SubscriptionSection = () => {
   return (
@@ -10,14 +11,16 @@ const SubscriptionSection = () => {
         <p className="text-lg mb-6">
           Join the Mizzle revolution today and be part of the millions who've already embraced a brighter future!
         </p>
-        <div className="flex  border py-4 max-w-[550px] bg-white justify-center rounded-md">
+        <div className="flex md:flex-row items-center gap-8   py-4 max-w-[550px] bg-white justify-center rounded-md">
           <input
             type="email"
             placeholder="Your email address"
-            className="w-64 px-4 py-2 rounded-l-md text-gray-900 "
+            className="md:w-64 w-40 px-4 py-2 rounded-l-md text-gray-900  "
           />
-          <button className="bg-green-600 px-6 py-2 rounded-md hover:bg-green-500">
-            Join Whatsapp Community
+          <button className="bg-green-600 px-6 gap-1 flex items-center py-2 rounded-md hover:bg-green-500">
+           
+            <IoLogoWhatsapp/>
+            Join 
           </button>
         </div>
       </div>
